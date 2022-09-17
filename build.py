dc3b8bf6eb4d9473b73376bec64db3cd9f56fce7
Ryan Luchs <rluchs107@gmail.com>
2022-09-17 00:05:01 -0500

    src/json: created templates for features
    Created templates for stand alone json file features, also added to build.py
    so that it will report json parsing error when building definitions.js, though
    currently is just discards the result.
    
    Signed-off-by: Ryan Luchs <rluchs107@gmail.com>

--- build.py
+++ build.py
@@ -1,27 +1,55 @@
 #!/usr/bin/env python3
 
+import sys
 import json
 
 from pathlib import Path
 
+def error_context(data, error, context):
+
+	lines  = data.split("\n")
+	center = error.lineno - 1
+	start  = max(center - context, 0);
+	stop   = min(center + context, len(lines) - 1);
+	digits = len(str(error.lineno))
+	out    = "%%%dd: %%s" % digits
+	text   = []
+
+	for index in range(start, stop + 1):
+		text.append(out % (index + 1, lines[index]))
+		if index == center:
+			text.append("-" * (digits + error.colno + 1) + "^")
+
+	return "\n".join(text)
+
 def compile_definitions():
 
-	compiled = json.loads(Path("./src/json/template.json").read_text())
+	compiled = json.loads(Path("./src/json/definitions.json").read_text())
 
 	# compile all of the json data into a monolithic file
 	for file in Path("./src/json").iterdir():
 		
-		if not file.is_dir():
+		# we want every folder except the templates
+		if not file.is_dir() or file.name == "templates":
 			continue
 
 		definitions = []
 
 		for definition in file.iterdir():
-			definitions.append(json.loads(definition.read_text()))
+
+			data = definition.read_text()
+
+			try:
+				definitions.append(json.loads(data))
+			except json.decoder.JSONDecodeError as error:
+				print("Error loading: " + str(definition), file=sys.stderr)
+				print(error_context(data, error, 3), file=sys.stderr)
+				print(error, file=sys.stderr)
+				sys.exit(1)
 
 		compiled[file.name] = definitions
 
-	print(json.dumps(compiled, indent=2))
+	# print(json.dumps(compiled, indent=2))
 	return compiled
 
 def main():
@@ -30,11 +58,15 @@ def main():
 	definitions_literal  = json_template_string
 	definitions          = json.loads(definitions_literal);
 
-	compile_definitions()
-
 	Path("definitions.js").write_text(
 		f"const definitions = {json_template_string};"
 	)
+
+	# compiled = compile_definitions()
+
+	# Path("definitions_.js").write_text(
+	# 	f"const definitions = {json.dumps(compiled, indent=2)};"
+	# )	
 
 if __name__ == "__main__":
 	main()
